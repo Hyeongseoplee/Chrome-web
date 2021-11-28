cde1379f76359ddf131112154563505f3214268e
vennylee <hyungsup42134@gmail.com>
2021-11-29 04:39:17 +0900

    Add background.js file on previous commit

---
+++ components/background.js
@@ -0,0 +1,10 @@
+"use srtict";
+
+const images = ["bg1.jpeg", "bg2.jpeg", "bg3.jpeg", "bg4.jpeg", "bg5.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bg = document.createElement("img");
+bg.setAttribute("class", "backgroundImg");
+bg.src = `/images/${chosenImage}`;
+document.body.appendChild(bg);
